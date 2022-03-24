c7ee406af407b555fb3b46ba6c9e205f6e19dd75
Yaron Haviv <yaronh@iguaz.io>
2022-03-25 01:32:19 +0200

    Update func.py

--- subdir/func.py
+++ subdir/func.py
@@ -17,17 +17,3 @@ def nuclio_handler(context, event):
                             headers={},
                             content_type='text/plain',
                             status_code=200)
-
-# mlrun serving runtime entry
-def echo(event):
-    myfunc()
-    return f'tag={tag}'
-  
-# serving runtime hooks
-from mlrun.runtimes import nuclio_init_hook
-
-def init_context(context):
-    nuclio_init_hook(context, globals(), "serving_v2")
-
-def handler(context, event):
-    return context.mlrun_handler(context, event)
