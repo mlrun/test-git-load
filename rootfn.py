36ed923cbf5ec96f2e568fc713ee8c44200071ac
Yaron Haviv <yhaviv@gmail.com>
2022-03-28 14:05:02 +0300

    add funcs

---
+++ rootfn.py
@@ -0,0 +1,19 @@
+# test loading repo from git in different mlrun runtimes
+# contain function code (this) and locally imported lib (mylib)
+
+from mylib import myfunc
+
+tag = "main"
+
+# mlrun job runtime entry
+def job_handler(context):
+  myfunc()
+  context.log_result("tag", tag)
+
+# mlrun nuclio runtime entry
+def nuclio_handler(context, event):
+    myfunc()
+    return context.Response(body=f'tag={tag}',
+                            headers={},
+                            content_type='text/plain',
+                            status_code=200)
